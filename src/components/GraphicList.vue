216a501a9a42affdd122b152cb3ec89535033227
Kate <curran.kate.t@gmail.com>
2019-01-16 11:38:59 +0100

    changes to graphic list

--- src/components/GraphicList.vue
+++ src/components/GraphicList.vue
@@ -1,11 +1,17 @@
 <template>
-  <div id="graphicList ">
+  <!--  <div class="graphicList">
     <ul class="movieList">
-      <li v-for="(data, index) in movies" :key="index" class="movieItem ">
-        <img v-bind:src="data.image" class="movie-image">        
-        
+      <li v-for="(data, index) in movies" :key="index" class="movieItem">
+        <img v-bind:src="data.images[0]" class="movie-image">
       </li>
     </ul>
+  </div>-->
+  <div class="graphicList">
+    <ul class="movieList container-fluid">
+      <li v-for="(data, index) in movies" :key="index" class="movieItem col-xs-6">
+        <img v-bind:src="data.images[0]" class="image-fluid">
+      </li>
+    </ul> 
   </div>
 </template>
 
@@ -18,152 +24,234 @@ export default {
     return {
       movies: [
         {
-          title: "Call me by your name",
-          productionCountries: ["Italien", "USA"],
-          productionYear: 2017,
-          length: 132,
+          title: "Gallowwalkers",
+          productionCountries: ["Italy", "Sweden"],
+          productionYear: 2019,
+          length: 120,
+          genre: "Action|Horror|Sci-Fi|Western",
+          distributor: "Holdlamis",
+          language: "Kashmiri",
+          subtitles: "Tetum",
+          director: "Mei Edlyn",
+          actors: [
+            "Jannie Kekelen",
+            "Annie Löfven",
+            "Peter Skogstokig",
+            "Kalkonen Adamsson"
+          ],
+          description:
+            "in eleifend quam a odio in hac habitasse platea dictumst maecenas ut massa quis augue luctus tincidunt nulla mollis molestie lorem quisque ut erat curabitur gravida nisi at nibh in hac habitasse platea dictumst aliquam augue quam sollicitudin",
+          images: [
+            "https://m.movieposter.com/posters/archive/main/226/MPW-113357",
+            "@/assets/movies/Gallowwalkers/poster-2.jpeg",
+            "@/assets/movies/Gallowwalkers/poster-3.jpeg"
+          ],
+          youtubeTrailers: ["https://www.youtube.com/watch?v=_frXIZZSaO4"],
+          reviews: [
+            {
+              source: "Svenska Dagbladet",
+              quote: "En film som får en att äta asmycket smörpopcorn",
+              stars: 4,
+              max: 5
+            },
+            {
+              source: "Svenska Nattbladet",
+              quote: "Enastående film, härliga subtitles",
+              stars: 3,
+              max: 5
+            },
+            {
+              source: "Svenska Eftermiddagsbladet",
+              quote: "Fantastiskt landskap, fin CGI",
+              stars: 4,
+              max: 5
+            }
+          ],
+          ageRestriction: 8,
+          price: 100
+        },
+        {
+          title: "Emperor's New Groove 2: Kronk's New Groove",
+          productionCountries: ["Spain", "USA"],
+          productionYear: 2019,
+          length: 115,
+          genre: "Animation|Children|Comedy",
+          distributor: "Flexidy",
+          language: "Finnish",
+          subtitles: "Burmese",
+          director: "Carley Gerrietz",
+          actors: ["Stefan Kvistholm", "Makuna Hatata", "Petra Torgstokig"],
+          description:
+            "vitae mattis nibh ligula nec sem duis aliquam convallis nunc proin at turpis a pede posuere nonummy integer non velit donec diam neque vestibulum eget vulputate ut ultrices vel augue",
+          images: [
+            "https://m.movieposter.com/posters/archive/main/226/MPW-113357",
+            "@/assets/movies/Emperors-Groove/poster-2.jpeg",
+            "@/assets/movies/Emperors-Groove/poster-3.jpeg"
+          ],
+          youtubeTrailers: ["https://www.youtube.com/watch?v=Hjvy8vc39kw"],
+          reviews: [
+            {
+              source: "Svenska Dagbladet",
+              quote: "Petra Torgstokig sjunger fantastiskt vackert",
+              stars: 5,
+              max: 5
+            },
+            {
+              source: "Svenska Nattbladet",
+              quote: "Rörande, även barnen grät",
+              stars: 4,
+              max: 5
+            },
+            {
+              source: "Svenska Eftermiddagsbladet",
+              quote: "Helt ok, men så mycket mer",
+              stars: 3,
+              max: 5
+            }
+          ],
+          ageRestriction: 12,
+          price: 100
+        },
+        {
+          title: "Luzhin Defence",
+          productionCountries: ["Brazil", "USA"],
+          productionYear: 2019,
+          length: 114,
+          genre: "Drama|Romance",
+          distributor: "Tresom",
+          language: "Swedish",
+          subtitles: "Kannada",
+          director: "Rosemonde Hemeret",
+          actors: [
+            "Lady Gaga",
+            "Ulf Löfven",
+            "Peder Tokskogs",
+            "Tiger Lenasson"
+          ],
+          description:
+            "hac habitasse platea dictumst maecenas ut massa quis augue luctus tincidunt nulla mollis molestie lorem quisque ut erat curabitur gravida nisi at nibh in hac",
+          images: [
+            "https://m.movieposter.com/posters/archive/main/226/MPW-113357",
+            "@/assets/movies/Luzhin-Defence/poster-2.jpeg",
+            "@/assets/movies/Luzhin-Defence/poster-3.jpeg"
+          ],
+          youtubeTrailers: ["https://www.youtube.com/watch?v=9BeWpZVVDhw"],
+          reviews: [
+            {
+              source: "Svenska Dagbladet",
+              quote: "Om man gillar shack är detta filmen för dig",
+              stars: 2,
+              max: 5
+            },
+            {
+              source: "Svenska Nattbladet",
+              quote: "En enastående shackfilm för shacklovers",
+              stars: 3,
+              max: 5
+            },
+            {
+              source: "Svenska Eftermiddagsbladet",
+              quote: "Inte passande för barn",
+              stars: 2,
+              max: 5
+            }
+          ],
+          ageRestriction: 16,
+          price: 100
+        },
+        {
+          title: "So Ends Our Night",
+          productionCountries: ["France", "Belgium"],
+          productionYear: 2019,
+          length: 127,
+          genre: "Drama|War",
+          distributor: "Zoolab",
+          language: "Yiddish",
+          subtitles: "Estonian",
+          director: "Reagen Gunney",
+          actors: [
+            "Erika Neandersson",
+            "Andres Kelpity",
+            "Pedro Pedriotto",
+            "Cattis Puma"
+          ],
+          description:
+            "morbi non quam nec dui luctus rutrum nulla tellus in sagittis dui vel nisl duis ac nibh fusce lacus purus aliquet at feugiat non pretium quis lectus suspendisse potenti in eleifend quam a odio in hac habitasse platea dictumst",
+          images: [
+            "https://m.movieposter.com/posters/archive/main/226/MPW-113357",
+            "@/assets/movies/So-Ends-Our-Night/poster-2.jpeg",
+            "@/assets/movies/So-Ends-Our-Night/poster-3.jpeg"
+          ],
+          youtubeTrailers: ["https://www.youtube.com/watch?v=O5ROSS9ReUY"],
+          reviews: [
+            {
+              source: "Svenska Dagbladet",
+              quote: "Ganska rolig, mycket blod",
+              stars: 2,
+              max: 5
+            },
+            {
+              source: "Svenska Nattbladet",
+              quote: "Vi gillade att det var så mycket blod",
+              stars: 5,
+              max: 5
+            },
+            {
+              source: "Svenska Eftermiddagsbladet",
+              quote: "Krig när det är som bäst",
+              stars: 4,
+              max: 5
+            }
+          ],
+          ageRestriction: 11,
+          price: 100
+        },
+        {
+          title: "Declaration of War",
+          productionCountries: ["Denmark", "Norway"],
+          productionYear: 2019,
+          length: 101,
           genre: "Drama",
-          distributor: "UIP",
-          language: "engelska",
-          subtitles: "svenska",
-          director: "Luca Guadagnino",
-          actors: ["Armie Hammer", "Timothée Chalamet", "Michael Stuhlbarg"],
+          distributor: "Wrapsafe",
+          language: "Estonian",
+          subtitles: "Lao",
+          director: "Neils Curnock",
+          actors: [
+            "Jan-Olof Strömqvist",
+            "Anna Blad",
+            "Pedra Blomquist",
+            "Labradoro Knutsson"
+          ],
           description:
-            "Filmen utspelas i norra Italien sommaren 1983. En ung amerikansk-italienare blir förälskad i en amerikansk student som kommer för att studera och bo hos hans familj. Tillsammans upplever de en oförglömlig sommar - full av musik, mat och kärlek - som för evigt kommer att förändra dem.",
-          image:
-            "https://www.mauvais-genres.com/22093/call-me-by-your-name-movie-poster-15x21-in-2017-luca-guadagnino-armie-hammer.jpg",
-          youtubeTrailers: ["Z9AYPxH5NTM"],
+            "rhoncus dui vel sem sed sagittis nam congue risus semper porta volutpat quam pede lobortis ligula sit amet eleifend pede libero quis orci nullam molestie nibh in lectus pellentesque at nulla suspendisse potenti cras",
+          images: [
+            "https://m.movieposter.com/posters/archive/main/226/MPW-113357",
+            "@/assets/movies/Declaration-of-War/poster-2.jpeg",
+            "@/assets/movies/Declaration-of-War/poster-3.jpeg"
+          ],
+          youtubeTrailers: ["https://www.youtube.com/watch?v=v5NMhtOTC-s"],
           reviews: [
             {
-              source: "Sydsvenskan",
-              quote: "ett drama berättat med stor ömhet",
-              stars: 4,
-              max: 5
-            },
-            {
               source: "Svenska Dagbladet",
-              quote: "en film att förälska sig i",
-              stars: 5,
+              quote: "Realistisk krigsfilm, vi är traumatiserade",
+              stars: 4,
               max: 5
             },
             {
-              source: "DN",
-              quote: "en het romans i åttiotalskostym",
-              stars: 4,
+              source: "Svenska Nattbladet",
+              quote: "För många huvudkaraktärer dog tidigt i filmen",
+              stars: 3,
+              max: 5
+            },
+            {
+              source: "Svenska Eftermiddagsbladet",
+              quote: "Trevligt regi av Neils Curnock",
+              stars: 2,
               max: 5
             }
-          ]
-        },
-        {
-          title: "Fightclub",
-          productionCountries: ["Italien", "USA"],
-          productionYear: 2017,
-          length: 132,
-          genre: "Action Love Story",
-          distributor: "UIP",
-          language: "engelska",
-          subtitles: "svenska",
-          director: "Luca Guadagnino",
-          actors: ["Armie Hammer", "Timothée Chalamet", "Michael Stuhlbarg"],
-          description:
-            "Angry men hit each other a lot. Woman cleans up after men and saves him, as per usual",
-          image:
-            "https://i.pinimg.com/originals/dd/ca/f3/ddcaf376aa330c99b80da5db39c41b6a.jpg",
-          youtubeTrailers: ["Z9AYPxH5NTM"],
-          reviews: [
-            {
-              source: "Sydsvenskan",
-              quote: "ett drama berättat med stor ömhet",
-              stars: 4,
-              max: 5
-            },
-            {
-              source: "Svenska Dagbladet",
-              quote: "en film att förälska sig i",
-              stars: 5,
-              max: 5
-            },
-            {
-              source: "DN",
-              quote: "en het romans i åttiotalskostym",
-              stars: 4,
-              max: 5
-            }
-          ]
-        },
-        {
-          title: "Batman",
-          productionCountries: ["Italien", "USA"],
-          productionYear: 2017,
-          length: 132,
-          genre: "Action Love Story",
-          distributor: "UIP",
-          language: "engelska",
-          subtitles: "svenska",
-          director: "Luca Guadagnino",
-          actors: ["Armie Hammer", "Timothée Chalamet", "Michael Stuhlbarg"],
-          description:
-            "Angry men hit each other a lot. Woman cleans up after men and saves him, as per usual",
-          image:
-            "https://i.embed.ly/1/display/resize?width=800&height=800&key=3e750996b20f47be9451da09d3fffa5b&url=https%3A%2F%2Fi.kinja-img.com%2Fgawker-media%2Fimage%2Fupload%2Fs--OgfqF89X--%2Fc_scale%2Cfl_progressive%2Cq_80%2Cw_800%2Fdrdfgdfjttt1tn32zuck.jpg",
-          youtubeTrailers: ["Z9AYPxH5NTM"],
-          reviews: [
-            {
-              source: "Sydsvenskan",
-              quote: "ett drama berättat med stor ömhet",
-              stars: 4,
-              max: 5
-            },
-            {
-              source: "Svenska Dagbladet",
-              quote: "en film att förälska sig i",
-              stars: 5,
-              max: 5
-            },
-            {
-              source: "DN",
-              quote: "en het romans i åttiotalskostym",
-              stars: 4,
-              max: 5
-            }
-          ]
-        },
-        {
-          title: "Aquaman",
-          productionCountries: ["Italien", "USA"],
-          productionYear: 2017,
-          length: 132,
-          genre: "Action Love Story",
-          distributor: "UIP",
-          language: "engelska",
-          subtitles: "svenska",
-          director: "Luca Guadagnino",
-          actors: ["Armie Hammer", "Timothée Chalamet", "Michael Stuhlbarg"],
-          description:
-            "Angry men hit each other a lot. Woman cleans up after men and saves him, as per usual",
-          image:
-            "https://images-na.ssl-images-amazon.com/images/I/51MDpDiZKOL.jpg",
-          youtubeTrailers: ["Z9AYPxH5NTM"],
-          reviews: [
-            {
-              source: "Sydsvenskan",
-              quote: "ett drama berättat med stor ömhet",
-              stars: 4,
-              max: 5
-            },
-            {
-              source: "Svenska Dagbladet",
-              quote: "en film att förälska sig i",
-              stars: 5,
-              max: 5
-            },
-            {
-              source: "DN",
-              quote: "en het romans i åttiotalskostym",
-              stars: 4,
-              max: 5
-            }
-          ]
+          ],
+          ageRestriction: 16,
+          price: 100
         }
       ]
     };
@@ -171,19 +259,26 @@ export default {
 };
 </script>
 
-<style scoped>
+<style>
+.graphicList {
+  max-height: 100vh;
+  display: flex;
+}
+
 .movieList {
-    display: flex;
-    width: 100vw;
-    list-style: none;
-    justify-content: space-evenly;
+  display: flex;
+  list-style: none;
+  justify-content: center;
+}
+
+.movieItem {
+  flex: 1;
 }
 
 .movie-image {
   max-width: 100%;
-  height: 30%;
-  border: 8px black solid;
+  height: 100%;
+  border: 6px black solid;
   object-fit: cover;
 }
-
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="home">
-    <Home />
     <GraphicList />
   </div>
 </template>
