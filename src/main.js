1d6bb10f59891b8bcdb5892e82ffd27f89b14f1a
Aarkan <johanwiren1@gmail.com>
2019-01-17 09:37:16 +0100

    cleanup

--- src/main.js
+++ src/main.js
@@ -25,11 +25,13 @@ db.ref(refMovies).on("value", getData, errData);
 db.ref(refAudits).on("value", getData, errData);
 db.ref(refShows).on("value", getData, errData);
 
+// count all callbacks before starting Vue
 let callbackCounter = 0;
 
 // callback when getting data
 function getData(data) {
   let gotJSON = data.val();
+
   callbackCounter++;
   // the same callback is called on all three
   // ref connections
@@ -37,7 +39,6 @@ function getData(data) {
   switch (data.ref.key) {
     case refMovies:
       Vue.$store.movies = gotJSON;
-      Vue.$store.ready = true;
       break;
     case refAudits:
       Vue.$store.auditoriums = gotJSON;
@@ -48,6 +49,8 @@ function getData(data) {
     default:
   }
 
+  // load all data from firebase before
+  // starting the Vue app
   if (callbackCounter === 3) {
     new Vue({
       router,
--- src/views/Examples.vue
+++ src/views/Examples.vue
@@ -1,29 +1,19 @@
 <template>
   <div class="examples container row mx-auto content-bg">
     <div class="col-12 pt-3">
-
-    <MovieInformation />
-
+      <MovieInformation/>
     </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import Example1 from "@/components/examples/Example1.vue";
-import Example2 from "@/components/examples/Example2.vue";
 import MovieInformation from "@/components/MovieInformation.vue";
 
 export default {
   name: "examples",
   components: {
-    Example1,
-    Example2,
     MovieInformation
-  },
-  created(){
-    console.log('test global from examples');
-    console.log(this.$store.movies); 
   }
 };
 </script>
