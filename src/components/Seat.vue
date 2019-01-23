28107412d7c66378f76eba073b8e38964eaa505d
Elliott <dahviimakes@gmail.com>
2019-01-23 13:18:35 +0100

    val av säten - nu kan en välja ett visst antal säten och inte mer  och kan avvälja säten

--- src/components/Seat.vue
+++ src/components/Seat.vue
@@ -1,56 +1,85 @@
 <template>
-  <div class="seat" :class="{ selected: this.clicked }"  v-on:click="clickSeat" >
+  <div class="seat" :class="{ selected: this.selected }"  v-on:click="clickSeat" >
   </div>
 </template>
 
 <script>
+/*TO DO:
+ * class: blocked  - enabled select för de sätena 
+*/
 export default {
   name: "seat",
   data(){
     return{
-      clicked: false
+      selected: false
     }
   },
   props: {
     row: Number,
     seatNr: Number,
     empty: Number,
+    selectedTickets: Number,
   },
   methods:{
-    clickSeat(){
-      this.clicked = this.clicked ? false : true; 
-      let sendClick = {
+    clickSeat(){          
+      //om användaren har säten kvar att välja (valt fler biljetter än säten)  
+      if(this.$parent.numberOfSelectedSeats  < this.$parent.selectedTickets){ //selectedTickets variabel ska fås från anders        
+        this.selected = this.selected ? false : true; 
+        let seatInfo = this.getSeatInfo();
+        this.$emit('click-seat', seatInfo);
+      }
+      //om användaren har valt lika många säten som biljeter - kan ta bort säten men inte välja fler
+     else {
+        this.selected=false;
+        let seatInfo = this.getSeatInfo();
+        this.$emit('remove-seat', seatInfo);
+      }
+    },//clickSeat
+
+    getSeatInfo(){
+      let seatInfo = {
         row: this.row,
         seatNr: this.seatNr,
-        clicked: this.clicked
+        selected: this.selected,
       }
-      this.$emit('click-seat', sendClick);
-      //this.clicked=true;
-
-      // alert(`Row: ${this.row}, SeatNr: ${this.seatNr}, Empty?: ${!this.empty ? 'Yes' : 'No'}`);
-    }
-  }
-  
-};
+      return seatInfo;
+    }//getSeatInfo
+  }//methods 
+};//export default
 </script>
 
 <style scoped>
+:root { 
+    --main-background-color: whitesmoke;       /*whitesmoke is for background and text inside special elements*/ 
+    --main-element-color: #36454f; /*CHARCOAL/       /Charcoal is for text on main pages and navbar background*/   
+    --special-element-color: #b08a43; /*"BRASS"/       /Brass is for special elements like buttons*/   
+    --main-font-family: 'Montserrat', sans-serif;    
+    /* Use global color-variables with:     EXAMPLE: "color: var(--main-element-color);" */
+    }
 .seat{
   display: inline-block;
   width: 4vmin;
   height: 4vmin;
   margin: 2px;
-  background-color: grey;
+  background-color: var(--main-element-color);
   border-top-right-radius: 50%;
   border-bottom-right-radius: 50%;
 }
 .seat:hover{
   cursor: pointer;
-    background-color: blue;
 }
 
 .seat.selected{
-    background-color: brown;
+    background-color:  var(--special-element-color);
 
 }
+.seat.blocked{
+    background-color:  red;
+
+}
+/*@media screen and (min-width:800px){
+  .seat:hover{
+    background-color: #4e6372;
+  }
+}*/
 </style>
--- src/components/SeatsComponent.vue
+++ src/components/SeatsComponent.vue
@@ -4,7 +4,8 @@
 <div v-for="(row, index) in auditorium" :key="row + index">
     <div v-for="(seat, seatNr) in auditorium[index]" :key="seat+ seatNr">
          <Seat v-bind:row="index + 1" v-bind:seatNr="seatNr + 1" v-bind:empty="seat"
-         v-on:click-seat="clickedSeat($event)"/>
+         v-on:click-seat="selectSeat($event)" v-on:remove-seat="unSelectSeat($event)"
+         />
     </div>
 </div>
 
@@ -13,69 +14,48 @@
 
 <script>
 import Seat from "@/components/Seat.vue";
-// import Vue from 'vue'; 
 export default {
     name: "seatsComponent",
     data() {
         return {
         auditorium: null,
-        //auditoriums:this.$store.auditoriums,
-        //seatsPerRow nedan endast för utvecklingssyften och testning
-        //
-        clickedSeats: []
+        selectedTickets: 3,
+        numberOfSelectedSeats:0,
+        selectedSeats: []
         };
-    },
+    },//data
     methods: {
-        clickedSeat(data){
-            if (!data.clicked){ //om sätet redan är klickat (förbokat)
-                for(let i = 0 ; i < this.clickedSeats.length ; i++)
-                    if(this.clickedSeats[i].seatNr === data.seatNr && this.clickedSeats[i].row === data.row){
-                        this.clickedSeats.splice(i, 1);
-                        break;
-                    }
-            }
-            else{
-                this.clickedSeats.push(data);
-            }
-            console.log(this.clickedSeats);
-            
-            //alert(`Row: ${data.row}, SeatNr: ${data.seatNr}`);
+        selectSeat(data){
+                if (!data.selected){ //om sätet redan är klickat (förbokat)
+                    this.unSelectSeat(data);
+                }
+                else{ //select seat by adding it to our array
+                    this.selectedSeats.push(data);
+                    this.numberOfSelectedSeats++;
+                                    console.log(this.selectedSeats);
 
-        }
-    },
+                }
+        },//selectSeat
+
+        unSelectSeat(data){
+            for(let i = 0 ; i < this.selectedSeats.length ; i++){
+                if(this.selectedSeats[i].seatNr === data.seatNr && this.selectedSeats[i].row === data.row){
+                    this.selectedSeats.splice(i, 1);
+                    this.numberOfSelectedSeats--;  
+                }
+            } 
+                console.log(this.selectedSeats);
+        }//unSelectSeat
+    }, //methods
+
     created() {
         this.auditorium = this.$store.shows["-LWl4d2Fs9Y-q1RqYgDZ"].shows[0].auditorium.seats;
-
-
-
-/*
-        // konstruerar en array med seat-objekt 
-        let SeatClass= Vue.extend(Seat)
-        this.seatsArray = [];
-        for (let i = 0; i < this.seatsPerRow.length; i++) {
-            let rowArray= [];
-            for(let j= 0; j < this.seatsPerRow[i]; j++){
-                //pushar in ett Seat-objekt med värden baserat på platsen
-                rowArray.push(new SeatClass({ 
-                    propsData: { //initieringsvärden
-                        row: i+1,
-                        seatNr: j+1,
-                        empty: false
-                    }
-                }));//rowArray push
-            }//for
-            this.seatsArray.push(rowArray);
-        }//for
-        console.log('seatsArray', this.seatsArray);
-        //console.log(this.seatsArray[0][0].empty);
-        */
-
     },//created
+
     components: {
         Seat
-  },
-
-};
+    },//components
+};//export default
 </script>
 
 <style scoped>
@@ -90,6 +70,4 @@ export default {
 #auditorium-seats > div {
 display: inline-block;
 }
-
-
 </style>
