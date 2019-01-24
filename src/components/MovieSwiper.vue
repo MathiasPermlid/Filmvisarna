38fb5c51d2513ed71627dbb7e88e2312ae2c43a7
Mathias Permlid <mathias.permlid@gmail.com>
2019-01-24 22:03:17 +0100

    Changed swiped button color

--- src/components/MovieSwiper.vue
+++ src/components/MovieSwiper.vue
@@ -86,17 +86,19 @@ export default {
 .posters img {
   height: auto;
   width: auto;
-  max-width: 30.5vh;
-  max-height: 30.5vh;
+  max-width: 30vh;
+  max-height: 30vh;
 }
 .swiper-button-prev {
   background-color: rgba(255, 255, 255, 0.418);
   border-radius: 40%;
+  background-image: url("data:image/svg+xml;charset=utf-8,%3Csvg%20xmlns%3D'http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg'%20viewBox%3D'0%200%2027%2044'%3E%3Cpath%20d%3D'M0%2C22L22%2C0l2.1%2C2.1L4.2%2C22l19.9%2C19.9L22%2C44L0%2C22L0%2C22L0%2C22z'%20fill%3D'%23eff0f1'%2F%3E%3C%2Fsvg%3E") !important;
 }
 
 .swiper-button-next {
   background-color: rgba(255, 255, 255, 0.418);
   border-radius: 50%;
+  background-image: url("data:image/svg+xml;charset=utf-8,%3Csvg%20xmlns%3D'http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg'%20viewBox%3D'0%200%2027%2044'%3E%3Cpath%20d%3D'M27%2C22L27%2C22L5%2C44l-2.1-2.1L22.8%2C22L2.9%2C2.1L5%2C0L27%2C22L27%2C22z'%20fill%3D'%23eff0f1'%2F%3E%3C%2Fsvg%3E") !important;
 }
 
 @media only screen and (max-width: 700px) {
@@ -118,7 +120,16 @@ export default {
   }
 
   .swiper-box {
-    border-width: 0 0 12vw;
+    border-width: 0 0 10vw;
   }
 }
 </style>
+
+
+.swiper-button-prev {
+  background-image: url("data:image/svg+xml;charset=utf-8,%3Csvg%20xmlns%3D'http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg'%20viewBox%3D'0%200%2027%2044'%3E%3Cpath%20d%3D'M0%2C22L22%2C0l2.1%2C2.1L4.2%2C22l19.9%2C19.9L22%2C44L0%2C22L0%2C22L0%2C22z'%20fill%3D'%23eff0f1'%2F%3E%3C%2Fsvg%3E") !important;
+}
+
+.swiper-button-next {
+  background-image: url("data:image/svg+xml;charset=utf-8,%3Csvg%20xmlns%3D'http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg'%20viewBox%3D'0%200%2027%2044'%3E%3Cpath%20d%3D'M27%2C22L27%2C22L5%2C44l-2.1-2.1L22.8%2C22L2.9%2C2.1L5%2C0L27%2C22L27%2C22z'%20fill%3D'%23eff0f1'%2F%3E%3C%2Fsvg%3E") !important;
+}
