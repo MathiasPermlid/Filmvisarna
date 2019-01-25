31731d4ad33269f4154f4023cf2e04fe7c754bc2
Mathias Permlid <mathias.permlid@gmail.com>
2019-01-25 10:39:49 +0100

    swiper changes

--- src/components/MovieSwiper.vue
+++ src/components/MovieSwiper.vue
@@ -62,7 +62,18 @@ export default {
   },
   created() {
     $(window).on("resize", () => {
-      this.swiperOption.slidesPerView = window.innerWidth < 700 ? 2.3 : 5.3;
+      let numOfPosters;
+      if (this.swiperOption.slidesPerView < 700) {
+        numOfPosters = 2.3;
+      } else if (
+        this.swiperOption.slidesPerView >= 700 &&
+        this.swiperOption.slidesPerView < 1500
+      ) {
+        numOfPosters = 5.3;
+      } else {
+        numOfPosters = 6.3;
+      }
+      return numOfPosters;
     });
   }
 };
@@ -92,13 +103,13 @@ export default {
 .swiper-button-prev {
   background-color: rgba(255, 255, 255, 0.418);
   border-radius: 40%;
-  background-image: url("data:image/svg+xml;charset=utf-8,%3Csvg%20xmlns%3D'http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg'%20viewBox%3D'0%200%2027%2044'%3E%3Cpath%20d%3D'M0%2C22L22%2C0l2.1%2C2.1L4.2%2C22l19.9%2C19.9L22%2C44L0%2C22L0%2C22L0%2C22z'%20fill%3D'%23eff0f1'%2F%3E%3C%2Fsvg%3E") !important;
+  background-image: url("data:image/svg+xml;charset=utf-8,%3Csvg%20xmlns%3D'http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg'%20viewBox%3D'0%200%2027%2044'%3E%3Cpath%20d%3D'M0%2C22L22%2C0l2.1%2C2.1L4.2%2C22l19.9%2C19.9L22%2C44L0%2C22L0%2C22L0%2C22z'%20fill%3D'%2336454f'%2F%3E%3C%2Fsvg%3E") !important;
 }
 
 .swiper-button-next {
   background-color: rgba(255, 255, 255, 0.418);
-  border-radius: 50%;
-  background-image: url("data:image/svg+xml;charset=utf-8,%3Csvg%20xmlns%3D'http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg'%20viewBox%3D'0%200%2027%2044'%3E%3Cpath%20d%3D'M27%2C22L27%2C22L5%2C44l-2.1-2.1L22.8%2C22L2.9%2C2.1L5%2C0L27%2C22L27%2C22z'%20fill%3D'%23eff0f1'%2F%3E%3C%2Fsvg%3E") !important;
+  border-radius: 40%;
+  background-image: url("data:image/svg+xml;charset=utf-8,%3Csvg%20xmlns%3D'http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg'%20viewBox%3D'0%200%2027%2044'%3E%3Cpath%20d%3D'M27%2C22L27%2C22L5%2C44l-2.1-2.1L22.8%2C22L2.9%2C2.1L5%2C0L27%2C22L27%2C22z'%20fill%3D'%2336454f'%2F%3E%3C%2Fsvg%3E") !important;
 }
 
 @media only screen and (max-width: 700px) {
@@ -125,11 +136,3 @@ export default {
 }
 </style>
 
-
-.swiper-button-prev {
-  background-image: url("data:image/svg+xml;charset=utf-8,%3Csvg%20xmlns%3D'http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg'%20viewBox%3D'0%200%2027%2044'%3E%3Cpath%20d%3D'M0%2C22L22%2C0l2.1%2C2.1L4.2%2C22l19.9%2C19.9L22%2C44L0%2C22L0%2C22L0%2C22z'%20fill%3D'%23eff0f1'%2F%3E%3C%2Fsvg%3E") !important;
-}
-
-.swiper-button-next {
-  background-image: url("data:image/svg+xml;charset=utf-8,%3Csvg%20xmlns%3D'http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg'%20viewBox%3D'0%200%2027%2044'%3E%3Cpath%20d%3D'M27%2C22L27%2C22L5%2C44l-2.1-2.1L22.8%2C22L2.9%2C2.1L5%2C0L27%2C22L27%2C22z'%20fill%3D'%23eff0f1'%2F%3E%3C%2Fsvg%3E") !important;
-}
